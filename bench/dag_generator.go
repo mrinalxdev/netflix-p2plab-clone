b3a9879af9b1306f22883634bd7c2e46020f716f
Mrinal Pramanick <mrinalbitsat@gmail.com>
2025-08-23 06:24:02 +0530

    Undergoing feat changes

--- bench/dag_generator.go
+++ bench/dag_generator.go
@@ -1,58 +1,83 @@
-// bench/dag_generator.go
 package bench
 
 import (
 	"fmt"
-	"time"
-
 	"ipld-benchmark/myipld"
+	"time"
 )
 
+type DAGStructure int
 
-func GenerateCustomDAG(numNodes int) (*myipld.MyNode, []*myipld.MyNode, error) {
+const (
+	LinearDAG DAGStructure = iota
+
+	BinaryTreeDAG
+	StarDAG
+	RandomDAG
+)
+
+func (d DAGStructure) String() string {
+	switch d {
+	case LinearDAG:
+		return "LinearDAG"
+	case BinaryTreeDAG:
+		return "BinaryTreeDAG"
+	case StarDAG:
+		return "StarDAG"
+	case RandomDAG:
+		return "RandomDAG"
+	default:
+		return "UnknownDAG"
+	}
+}
+
+func GenerateDAG(structure DAGStructure, numNodes int) (*myipld.MyNode, []*myipld.MyNode, error) {
+	switch structure {
+	case LinearDAG:
+		return GenerateLinearDAG(numNodes)
+	case BinaryTreeDAG:
+		return GenerateBinaryTreeDAG(numNodes)
+	case StarDAG:
+		return GenerateStarDAG(numNodes)
+	case RandomDAG:
+		return GenerateRandomDAG(numNodes, 3) // default maxLinks=3
+	default:
+		return nil, nil, fmt.Errorf("unknown DAG structure")
+	}
+}
+
+
+func GenerateLinearDAG(numNodes int) (*myipld.MyNode, []*myipld.MyNode, error){
 	if numNodes <= 0 {
 		return nil, nil, fmt.Errorf("numNodes must be positive")
 	}
 
-	var (
-		nodes    []*myipld.MyNode
-		prevNode *myipld.MyNode   
-		currNode *myipld.MyNode   
-		err      error
-	)
+	nodes := make([]*myipld.MyNode, 0, numNodes)
+	var prevNode *myipld.MyNode
 
 
-	leafData := map[string]interface{}{
-		"content": "custom-leaf-data-0",
-		"created": time.Now().UnixNano(),
-	}
-	leafNode, err := myipld.NewMyNode(leafData)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to create initial custom node: %w", err)
-	}
-	nodes = append(nodes, leafNode)
-	prevNode = leafNode
-
-	// creating subsequent nodes, each linking to the immediately previous node
-	for i := 1; i < numNodes; i++ {
+	for i := 0; i < numNodes; i++ {
 		nodeData := map[string]interface{}{
-			"index":     i,
+			"index" : i,
 			"timestamp": time.Now().UnixNano(),
-			"message":   fmt.Sprintf("custom-node-%d-data", i),
-		}
-		currNode, err = myipld.NewMyNode(nodeData)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to create custom node %d: %w", i, err)
+			"message" : fmt.Sprintf("node-%d-data", i),
 		}
 
-		linkName := fmt.Sprintf("custom-link-to-%x", prevNode.Cid.Hash[:8])
-		if err := currNode.AddLink(linkName, prevNode.Cid); err != nil {
-			return nil, nil, fmt.Errorf("failed to add link to custom node %d: %w", i, err)
+		currNode, err := myipld.NewMyNode(nodeData)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to create node %d : %w", i , err)
+		}
+
+
+		if prevNode != nil {
+			linkName := fmt.Sprintf("link-to-%w ", prevNode.Cid.Hash[:8])
+
+			if err := currNode.AddLink(linkName , prevNode.Cid); err != nil {
+				return nil, nil, fmt.Errorf("Failed to add link to node %d: %w", i, err)
+			}
 		}
 
 		nodes = append(nodes, currNode)
-		prevNode = currNode           
+		prevNode = currNode
 	}
-
-	return currNode, nodes, nil
 }
